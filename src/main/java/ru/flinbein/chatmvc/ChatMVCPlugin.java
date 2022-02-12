55d9346aeefdf7f1e1576a4f55f8def200906695
DPOH-VAR <dpohvar@gmail.com>
2022-02-13 04:49:24 +0500

    valid tags

--- src/main/java/ru/flinbein/chatmvc/ChatMVCPlugin.java
+++ src/main/java/ru/flinbein/chatmvc/ChatMVCPlugin.java
@@ -1,6 +1,30 @@
 package ru.flinbein.chatmvc;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import ru.flinbein.chatmvc.xml.MVCXmlParser;
 
 public class ChatMVCPlugin extends JavaPlugin {
+
+    MVCXmlParser parser = new MVCXmlParser();
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        try (var xmlStream = classLoader.getResourceAsStream("chatmvc_text.xml")){
+            BaseComponent msg = parser.parse(xmlStream);
+            sender.spigot().sendMessage(msg);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
 }
--- src/main/java/ru/flinbein/chatmvc/xml/MVCXmlParser.java
+++ src/main/java/ru/flinbein/chatmvc/xml/MVCXmlParser.java
@@ -2,7 +2,6 @@ package ru.flinbein.chatmvc.xml;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.*;
-import net.md_5.bungee.chat.ComponentSerializer;
 import org.w3c.dom.*;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -19,25 +18,28 @@ public class MVCXmlParser {
         } catch (Throwable ignored) {}
     }
 
-    static void parse(InputStream xml) throws Exception {
+    public BaseComponent parse(InputStream xml) throws Exception {
         // Создается построитель документа
 
         // Создается дерево DOM документа из файла
-        Document document = documentBuilder.parse("BookCatalog.xml");
+        Document document = documentBuilder.parse(xml);
         Element rootComponent = document.getDocumentElement();
+        return parseBaseComponent(rootComponent);
     }
 
-    private BaseComponent parseBaseComponent(Element element) {
-        String type = element.getTagName();
+    private BaseComponent parseBaseComponent(Element element){
+        return parseBaseComponent(element, element.getTagName());
+    }
+
+    private BaseComponent parseBaseComponent(Element element, String type) {
         BaseComponent component = switch (type) {
-            case "KeybindComponent" -> parseKeybindComponent(element);
-            case "TextComponent" -> parseTextComponent(element);
-            case "ScoreComponent" -> parseScoreComponent(element);
-            case "SelectorComponent" -> parseSelectorComponent(element);
-            case "TranslatableComponent" -> parseTranslatableComponent(element);
+            case "keybind" -> parseKeybind(element);
+            case "text" -> parseText(element);
+            case "score" -> parseScore(element);
+            case "selector" -> parseSelector(element);
+            case "translatable" -> parseTranslatable(element);
             default -> throw new RuntimeException("Unknown component - "+type);
         };
-        ComponentSerializer r;
 
         if (element.hasAttribute("bold")) {
             component.setBold(element.getAttribute("bold").equals("true"));
@@ -64,18 +66,106 @@ public class MVCXmlParser {
         if (element.hasAttribute("insertion")) {
             component.setInsertion( element.getAttribute("insertion") );
         }
+        Element hoverElement = null;
+        NodeList childNodes = element.getChildNodes();
+        for (var i=0; i<childNodes.getLength(); i++){
+            Node child = childNodes.item(i);
+            if (child instanceof Text text){
+                component.addExtra(text.getNodeValue().trim());
+            } else if (child instanceof Element e){
+                switch (e.getTagName()) {
+                    case "hover-entity", "hover-item", "hover-text" -> {
+                        if (hoverElement != null) throw new RuntimeException("Only one hover item allowed");
+                        hoverElement = e;
+                    }
+                    case "br" -> component.addExtra("\n");
+                    case "pre" -> component.addExtra(e.getNodeValue());
+                    case "space" -> component.addExtra(" ");
+                    default -> component.addExtra(parseBaseComponent(e));
+                }
+            } else {
+                throw new RuntimeException("ToDo: only text & component-tags allowed in tag <"+type+">");
+            }
+        }
+        if (element.hasAttribute("endl") && element.getAttribute("endl").equals("true")) {
+            component.addExtra("\n");
+        }
+
+        if (element.hasAttribute("onclick") && !element.getAttribute("onclick").isEmpty()) {
+            String onclickAttr = element.getAttribute("onclick");
+            String[] split = onclickAttr.split(":", 2);
+            if (split.length < 1) throw new RuntimeException("Wrong onclick value: "+onclickAttr);
+            String commandType = split[0];
+            String commandValue = split.length > 1 ? split[1] : "";
+            ClickEvent.Action action = switch (commandType) {
+                case "copy" -> ClickEvent.Action.COPY_TO_CLIPBOARD;
+                case "file" -> ClickEvent.Action.OPEN_FILE;
+                case "page" -> ClickEvent.Action.CHANGE_PAGE;
+                case "run" -> ClickEvent.Action.RUN_COMMAND;
+                case "suggest" -> ClickEvent.Action.SUGGEST_COMMAND;
+                case "url" -> ClickEvent.Action.OPEN_URL;
+                default -> throw new RuntimeException("Wrong onclick type: "+commandType);
+            };
+            component.setClickEvent(new ClickEvent(action, commandValue));
+        }
+
+        if (hoverElement != null) {
+            component.setHoverEvent(parseHoverEvent(hoverElement));
+        }
 
         return component;
     }
 
-    private KeybindComponent parseKeybindComponent(Element element) {
+    private HoverEvent parseHoverEvent(Element element){
+        return switch (element.getTagName()) {
+            case "hover-item" -> parseHoverEventItem(element);
+            case "hover-entity" -> parseHoverEventEntity(element);
+            case "hover-text" -> parseHoverEventText(element);
+            default -> throw new RuntimeException("Unknown hover element: "+element.getTagName());
+        };
+    }
+
+    private HoverEvent parseHoverEventItem(Element element){
+        String itemId = element.getAttribute("id");
+        int itemCount = element.hasAttribute("count") ? Integer.parseInt(element.getAttribute("count")) : 1;
+        ItemTag itemTag = null;
+        if (element.hasAttribute("tag")) {
+            itemTag = ItemTag.ofNbt(element.getAttribute("tag"));
+        }
+        var item = new net.md_5.bungee.api.chat.hover.content.Item(itemId, itemCount, itemTag);
+        return new HoverEvent(HoverEvent.Action.SHOW_ITEM, item);
+    }
+
+    private HoverEvent parseHoverEventEntity(Element element){
+        String id = element.getAttribute("id");
+        String type = element.getAttribute("type");
+        BaseComponent name = null;
+        if (element.hasAttribute("text") || element.getChildNodes().getLength() > 0) {
+            name = parseBaseComponent(element, "text");
+        }
+        var entity = new net.md_5.bungee.api.chat.hover.content.Entity(type, id, name);
+        return new HoverEvent(HoverEvent.Action.SHOW_ENTITY, entity);
+    }
+
+    private HoverEvent parseHoverEventText(Element element){
+        net.md_5.bungee.api.chat.hover.content.Text text;
+        if (element.hasAttribute("text") && element.getAttributes().getLength() == 1) {
+            text = new net.md_5.bungee.api.chat.hover.content.Text(element.getAttribute("text"));
+        } else {
+            BaseComponent base = parseBaseComponent(element, "text");
+            text = new net.md_5.bungee.api.chat.hover.content.Text(new BaseComponent[]{base});
+        }
+        return new HoverEvent(HoverEvent.Action.SHOW_TEXT, text);
+    }
+
+    private KeybindComponent parseKeybind(Element element) {
         KeybindComponent cmp = new KeybindComponent();
         String keybind = element.getAttribute("keybind");
         cmp.setKeybind(keybind);
         return cmp;
     }
 
-    private TextComponent parseTextComponent(Element element) {
+    private TextComponent parseText(Element element) {
         TextComponent cmp = new TextComponent();
         if (element.hasAttribute("text")) {
             cmp.setText(element.getAttribute("text"));
@@ -83,43 +173,41 @@ public class MVCXmlParser {
         return cmp;
     }
 
-    private ScoreComponent parseScoreComponent(Element element) {
+    private ScoreComponent parseScore(Element element) {
         String name = element.getAttribute("name");
         String objective = element.getAttribute("objective");
 
-        if (element.hasAttribute("value")) {
+        if (element.hasAttribute("value") && !element.getAttribute("value").isEmpty()) {
             return new ScoreComponent(name, objective, element.getAttribute("value"));
         }
 
         return new ScoreComponent(name, objective);
     }
 
-    private SelectorComponent parseSelectorComponent(Element element) {
+    private SelectorComponent parseSelector(Element element) {
         String selector = element.getAttribute("selector");
         return new SelectorComponent(selector);
     }
 
-    private TranslatableComponent parseTranslatableComponent(Element element) {
+    private TranslatableComponent parseTranslatable(Element element) {
         String translate = element.getAttribute("translate");
         TranslatableComponent cmp = new TranslatableComponent(translate);
         NodeList childNodes = element.getChildNodes();
         Element withElement = null;
         for (var i = 0; i < childNodes.getLength(); i++) {
             var node = childNodes.item(i);
-            if (! (node instanceof Element)) continue;
-            Element child = (Element) node;
-            if (child.getTagName().equals("With")) {
+            if (! (node instanceof Element childComponent)) continue;
+            if (childComponent.getTagName().equals("with")) {
                 if (withElement != null) throw new RuntimeException("ToDo: Only 1 <With> allowed");
-                withElement = child;
+                withElement = childComponent;
             };
         }
         if (withElement != null) {
             NodeList withElementChildren = withElement.getChildNodes();
             for (var i = 0; i < withElementChildren.getLength(); i++) {
                 var child = withElementChildren.item(i);
-                if (child instanceof Text) {
-                    var txt = child.getNodeValue().trim();
-                    cmp.addWith(txt);
+                if (child instanceof Text text) {
+                    cmp.addWith(text.getNodeValue().trim());
                 } else if (child instanceof Element) {
                     var childComponent = parseBaseComponent((Element) child);
                     cmp.addWith(childComponent);
