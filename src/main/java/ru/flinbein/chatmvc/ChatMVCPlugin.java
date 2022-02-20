043653c8b6fc0c7de2993d7d3364fb10d36cb119
DPOH-VAR <dpohvar@gmail.com>
2022-02-20 20:25:53 +0500

    fix ChatMVCPlugin tab-complete

--- src/main/java/ru/flinbein/chatmvc/ChatMVCPlugin.java
+++ src/main/java/ru/flinbein/chatmvc/ChatMVCPlugin.java
@@ -2,7 +2,11 @@ package ru.flinbein.chatmvc;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import ru.flinbein.chatmvc.handler.CommandHandler;
+
+import java.util.List;
 
 
 public class ChatMVCPlugin extends JavaPlugin {
@@ -12,5 +16,11 @@ public class ChatMVCPlugin extends JavaPlugin {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         return commandHandler.handleCommand(sender, args);
+    }
+
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        return commandHandler.handleTabComplete(sender, args);
     }
 }
